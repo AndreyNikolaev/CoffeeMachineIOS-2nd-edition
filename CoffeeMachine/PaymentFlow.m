7dbb0ffff43cd566de2e854e9639b58781940078
GracielaM <gracielamitkova@gmail.com>
2013-09-18 08:37:35 -0300

    .

--- CoffeeMachine/PaymentFlow.m
+++ CoffeeMachine/PaymentFlow.m
@@ -227,25 +227,7 @@
 
 -(void)rotateImage: (UIImageView*) image 
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> f43c8ab5705790fd52ba9203f5189f23c007b84e
-    /*[UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDelegate:self];
-    [UIView setAnimationDuration:2];
-    [UIView setAnimationDidStopSelector:@selector(moveImageBack:)];
-    [image.layer setValue:@-1.5707 forKeyPath:@"transform.rotation"];
-    [image.layer setValue:@0 forKeyPath:@"transform.scale.y"];
-    [UIView commitAnimations];
-     */
->>>>>>> 1c3d49af895a428070cecaafe03eb96ce294e2ca
->>>>>>> 256eb22e9291f1d2cc0543b014076ba0694d142e
         [UIView animateWithDuration:3.0 animations:^{
     
             [image.layer setValue:@-1.5707 forKeyPath:@"transform.rotation"];
@@ -259,54 +241,7 @@
         }];
     
           }
-<<<<<<< HEAD
 
-    
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
-=======
->>>>>>> f43c8ab5705790fd52ba9203f5189f23c007b84e
-    
-        [UIView animateWithDuration:1.0 animations:^{
-    
-[image.layer setValue:@-1.5707 forKeyPath:@"transform.rotation"];
-[image.layer setValue:@0 forKeyPath:@"transform.scale.y"];
-                      // [image.layer setValue:@1.5707 forKeyPath:@"transform.rotation"];
-           // [image.layer setValue:@-1.0 forKeyPath:@"transform.scale.y"];
-        } completion:nil ];
-
-   // image.center = self.oldCoinPosition;
->>>>>>> 1c3d49af895a428070cecaafe03eb96ce294e2ca
-
-    
-<<<<<<< HEAD
-=======
-    [UIView animateWithDuration:2.0
-                           delay:0.0f
-                         options:UIViewAnimationOptionCurveEaseOut animations:^{
-        [UIView animateWithDuration:3.0 animations:^{
-    
-[image.layer setValue:@-1.5707 forKeyPath:@"transform.rotation"];
-[image.layer setValue:@0 forKeyPath:@"transform.scale.y"];
-
-
-
-        } completion:nil];
-
-   } completion:nil];
-
-<<<<<<< HEAD
-}
-=======
->>>>>>> 8837cc06021fc087fb463388c66e5152ee4461a7
->>>>>>> f43c8ab5705790fd52ba9203f5189f23c007b84e
-
->>>>>>> 1c3d49af895a428070cecaafe03eb96ce294e2ca
->>>>>>> 256eb22e9291f1d2cc0543b014076ba0694d142e
 -(void)moveImageBack :(UIImageView*)image {
     NSLog(@"test");
     image.center = self.oldCoinPosition;
